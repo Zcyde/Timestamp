aefd7d132d48f60c015fd8d500283940188f3b9f
Zcyde <eldrinjoshpineda@gmail.com>
2025-08-25 14:46:05 +0800

    :Sparkle: Final Changes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // index.js
 // where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +17,41 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+/**
+ * Timestamp API
+ * Endpoint: /api/:date?
+ */
+app.get("/api/:date?", (req, res) => {
+  let dateParam = req.params.date;
+  let date;
 
+  // If no date param, use current date
+  if (!dateParam) {
+    date = new Date();
+  } else if (/^\d+$/.test(dateParam)) {
+    // If the string is all digits, treat it as UNIX timestamp (milliseconds)
+    date = new Date(parseInt(dateParam));
+  } else {
+    // Else, treat as date string
+    date = new Date(dateParam);
+  }
+
+  // Check for invalid date
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // Return response
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
